9a6859142daa379562d7d22731fc2763f6758662
wavicles <wavicles@users.noreply.github.com>
2014-06-11 01:02:51 +0530

    updated for accessing both sensors
    Modified the code for accessing both sensors simultaneously. They can be used to plot graphs for spring experiment. KE and PE or plotting two waves out of phase

--- srf2foe second sensor.py
+++ srf2foe second sensor.py
@@ -12,13 +12,19 @@ strt = time.time()
 
 et =0
 while et < 20:
-      dist = p.srfechotime(8,3)  
+      dist1 = p.srfechotime(8,3)
+      dist2 = p.srfechotime(9,0)
       et = time.time() - strt
       ta.append(et)
-      da.append(dist)
-      s = '%d'%(dist)
+      da1.append(dist1)
+      da2.append(dist2)
+      s = '%d'%(dist1)
+      ss = '%d'%(dist2)
       f.write(s + '\n')
+      f.write(ss + '\n')
       print s
+      print ss
       time.sleep(0.1)
-plot(ta,da)
+plot(ta,da1)
+plot(ts,da2)
 show()
