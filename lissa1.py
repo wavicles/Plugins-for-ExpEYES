e49c27f17b8699a4a21c6e8a1d9231b16e601f06
wavicles <wavicles@users.noreply.github.com>
2014-06-15 18:28:56 +0530

    Create lissa1.py
    Tried Plotting lissajous figures... this code will be useful for my sound experiments

---
+++ lissa1.py
@@ -0,0 +1,54 @@
+#import numpy functions and types
+from pylab import *
+
+t = arange(0, 2*pi, pi/400)
+x = cos(t)
+
+#case 1
+figure(1)
+n=1
+phi=0
+subplot(1,2,1)
+plot(cos(t),cos(n*t+phi),'r.')
+title('n=1, phi=0')
+
+#case 2
+n=1
+phi=pi/4
+subplot(1,2,2)
+plot(cos(t),cos(n*t+phi),'r.')
+title('n=1, phi=pi/4')
+
+figure(2)
+#case 3
+n=1
+phi=pi/2
+subplot(1,2,1)
+plot(cos(t),cos(n*t+phi),'r.')
+title('n=1, phi=pi/2')
+
+#case 4
+n=2
+phi=pi/2
+subplot(1,2,2)
+plot(cos(t),cos(n*t+phi),'r.')
+title('n=2, phi=pi/2')
+
+#case 5
+figure(3)
+n=4
+phi=pi/2
+subplot(1,2,1)
+plot(cos(t),cos(n*t+phi),'r.')
+title('n=4, phi=pi/2')
+
+#case 6
+n=8
+phi=-pi/3
+subplot(1,2,2)
+plot(cos(t),cos(n*t+phi),'r.')
+title('n=5, phi=-pi/3')
+savefig('plot_lissajous.png')
+
+#show() should be the last line
+show()
