5fdcafb513269fe7f608deff6b6b3eddaeeb173f
wavicles <wavicles@users.noreply.github.com>
2014-06-16 23:48:37 +0530

    Update srfdataplot.py

--- srfdataplot.py
+++ srfdataplot.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-data = np.loadtxt('srf.dat')
+data = np.loadtxt('srf.dat')  # uses data from srf.dat
 x1 = data[:,0]
 y1 = data[:,1]
 plt.plot(x1, y1)
