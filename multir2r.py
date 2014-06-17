5fa0c13777300bf062f043d81ad031707a4bb2e7
wavicles <wavicles@users.noreply.github.com>
2014-06-17 09:59:14 +0530

    Update multir2r.py

--- multir2r.py
+++ multir2r.py
@@ -9,3 +9,7 @@ t = p.multi_r2rtime(6,9)    # 6 is the readback of SQR1. Time of 10 cycles
 print t
 t = t * 1.0e-6  # to seconds
 print 'Frequency = ', (10.0/t)
+
+'''
+connect photogate led to SQR1 anf GND. Detector From SEn and GND
+'''
