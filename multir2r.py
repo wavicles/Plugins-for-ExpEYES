865209254129f7d2d6e301dca401fd70fb1e3fab
wavicles <wavicles@users.noreply.github.com>
2014-06-17 09:56:58 +0530

    Create multir2r.py
    Using multir2r function

---
+++ multir2r.py
@@ -0,0 +1,11 @@
+'''
+Program for measuring time intervals using photogates
+'''
+import expeyes.eyesj
+p = expeyes.eyesj.open()
+
+p.set_sqr1(1000)              # set 1kHz on  SQR1
+t = p.multi_r2rtime(6,9)    # 6 is the readback of SQR1. Time of 10 cycles
+print t
+t = t * 1.0e-6  # to seconds
+print 'Frequency = ', (10.0/t)
