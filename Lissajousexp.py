11e272c30b217dc67aca4f6d90694444632476e4
wavicles <wavicles@users.noreply.github.com>
2014-06-21 10:11:11 +0530

    Update Lissajousexp.py

--- Lissajousexp.py
+++ Lissajousexp.py
@@ -2,9 +2,8 @@ import expeyes.eyesj
 p = expeyes.eyesj.open()
 
 from pylab import *
-p.set_sqrs(150,25)  # Generate 150 Hz  on both with 90 degree phase shift (25% of T)
-
-res = p.capture2(6,7, 300, 100)  # returns two sets of data
-plot(res[0], res[1])   # SQR1 readback
-plot(res[2], res[3])   # SQR2 readback
+t1,t2,v1,v2 = p.capture2(1, 2, 300, 100)
+plot(t1, v1, t2, v2)
+plot(v1, v2)
 show()
+
