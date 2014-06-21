083b77849201af7aabde793b505e080e5d0ce93d
wavicles <wavicles@users.noreply.github.com>
2014-06-21 10:14:48 +0530

    Update Lissajousexp.py
    Two sine waves with a phase shift to produce Lissajous pattern

--- Lissajousexp.py
+++ Lissajousexp.py
@@ -1,3 +1,8 @@
+'''
+connect sin ti A1 and a 1 microfarad capacitor from A1 to A2. Connect 1K resistor from A2 to GND. Capture voltage before and after the capacitor.
+'''
+
+
 import expeyes.eyesj
 p = expeyes.eyesj.open()
 
