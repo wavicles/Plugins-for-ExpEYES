2a00fbfc932ecf1ae4cd133fe8e4cdb986c7e24c
wavicles <wavicles@users.noreply.github.com>
2014-06-22 11:29:50 +0530

    Update Lissajousexp.py

--- Lissajousexp.py
+++ Lissajousexp.py
@@ -7,7 +7,10 @@ import expeyes.eyesj
 p = expeyes.eyesj.open()
 
 from pylab import *
-t1,t2,v1,v2 = p.capture2(1, 2, 300, 100)
+t1,v1,t2,v2 = p.capture2(1, 2, 300, 100)
+figure(1)
+plot(t1,v1)
+
 plot(t1, v1, t2, v2)
 plot(v1, v2)
 show()
