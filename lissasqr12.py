4912d91b7a4e5698553d1ca4bab8dadf76a09743
wavicles <wavicles@users.noreply.github.com>
2014-06-23 10:02:29 +0530

    Create lissasqr12.py

---
+++ lissasqr12.py
@@ -0,0 +1,22 @@
+'''
+connect sqr1 to  A1 and and sqr2 to A2.
+'''
+
+from pylab import *
+import expeyes.eyesj
+p = expeyes.eyesj.open()
+
+
+#p.set_sqr1(8000)
+#p.set_sqr2(4000)
+p.set_sqrs(8000,13)
+
+
+t1,v1,t2,v2 = p.capture2(1, 2, 400, 16)
+figure(1)
+plot(t1,v1)
+figure(2)
+plot(t1,v1, t2,v2)
+figure(3)
+plot(v1,v2)
+show()
