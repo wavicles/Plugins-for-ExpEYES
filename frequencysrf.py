1a110bc58099c34e1058616c6a6c605b0bf063ae
wavicles <wavicles@users.noreply.github.com>
2014-06-24 09:56:20 +0530

    Create frequencysrf.py

---
+++ frequencysrf.py
@@ -0,0 +1,28 @@
+from pylab import *
+import expeyes.eyesj, time
+p = expeyes.eyesj.open()
+
+p.set_state(10,1)
+
+f = open('srf.dat','r+')
+
+ta = []
+da = []
+strt = time.time()
+
+et =0
+while et < 30:
+	dist = p.srfechotime(8,3)
+	et = time.time() - strt
+	ta.append(et)
+	da.append(dist)
+	s = '%5.3f\t %d'%(et,dist)
+	#s = '%d\t %d'%(et,dist)
+	f.write(s + '\n')
+	print s
+	time.sleep(0.1)
+
+print p.get_frequency(3) # to get frequency at pin 3
+
+plot(ta,da)
+show()
