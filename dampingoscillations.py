1d3dee9d196b1d5d293cc083e9d93583c75535d0
wavicles <wavicles@users.noreply.github.com>
2014-07-14 21:42:40 +0530

    Update dampingoscillations.py

--- dampingoscillations.py
+++ dampingoscillations.py
@@ -91,3 +91,54 @@ class Spring:
 
 	def msg(self,s, col = 'blue'):
 		msgwin.config(text=s, fg=col)
+
+p = eyes.open()
+p.disable_actions()
+root = Tk()
+Canvas(root, width = WIDTH, height = 5).pack(side=TOP)  # Some space at the top
+g = eyeplot.graph(root, width=WIDTH, height=HEIGHT, bip=False)	# make plot objects using draw.disp
+pen = Spring()
+
+cf = Frame(root, width = WIDTH, height = 10)
+cf.pack(side=TOP,  fill = BOTH, expand = 1)
+
+
+b3 = Label(cf, text = _('Digitize for'))
+b3.pack(side = LEFT, anchor = SW)
+DURATION = StringVar()
+Dur =Entry(cf, width=5, bg = 'white', textvariable = DURATION)
+DURATION.set('10')
+Dur.pack(side = LEFT, anchor = SW)
+b3 = Label(cf, text = _('Seconds.'))
+b3.pack(side = LEFT, anchor = SW)
+
+cf = Frame(root, width = WIDTH, height = 10)
+cf.pack(side=TOP,  fill = BOTH, expand = 1)
+b1 = Button(cf, text = _('START'), command = pen.start)
+b1.pack(side = LEFT, anchor = N)
+b1 = Button(cf, text = _('STOP'), command = pen.stop)
+b1.pack(side = LEFT, anchor = N)
+b1 = Button(cf, text = _('FIT'), command = pen.fit_curve)
+b1.pack(side = LEFT, anchor = N)
+b4 = Button(cf, text = _('CLEAR'), command = pen.clear)
+b4.pack(side = LEFT, anchor = N)
+b1 = Button(cf, text = _('Xmgrace'), command = pen.xmgrace)
+b1.pack(side = LEFT, anchor = N)
+b3 = Button(cf, text = _('SAVE to'), command = pen.save)
+b3.pack(side = LEFT, anchor = N)
+filename = StringVar()
+e1 =Entry(cf, width=15, bg = 'white', textvariable = filename)
+filename.set('oscillations.dat')
+e1.pack(side = LEFT)
+b5 = Button(cf, text = _('QUIT'), command = sys.exit)
+b5.pack(side = RIGHT, anchor = N)
+
+mf = Frame(root, width = WIDTH, height = 10)
+mf.pack(side=TOP)
+msgwin = Label(mf,text=_('Message'), fg = 'blue')
+msgwin.pack(side=LEFT, anchor = S, fill=BOTH, expand=1)
+
+
+#eyeplot.pop_image('pics/pend-wave.png', _('Pendulum Oscillations'))
+root.title(_('Damped oscillations of Spring in various media'))
+root.mainloop()
