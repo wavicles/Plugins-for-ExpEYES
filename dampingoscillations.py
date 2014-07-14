6784a7c53812a3cc683f372da0397eb0e903c096
wavicles <wavicles@users.noreply.github.com>
2014-07-14 21:41:29 +0530

    Create dampingoscillations.py

---
+++ dampingoscillations.py
@@ -0,0 +1,93 @@
+'''
+expEYES program
+Dampaed oscillations of spring in various media
+'''
+import gettext
+gettext.bindtextdomain("expeyes")
+gettext.textdomain('expeyes')
+_ = gettext.gettext
+
+from Tkinter import *
+import expeyes.eyesj as eyes, expeyes.eyeplot as eyeplot, expeyes.eyemath as eyemath, time, sys, math
+
+WIDTH  = 600   # width of drawing canvas
+HEIGHT = 400   # height    
+
+class Spring:
+	tv = [ [], [] ]			# Lists for Readings
+	TIMER = 5			# Time interval between reads
+	MINY = -5			# Voltage range
+	MAXY = 5
+	running = False
+	MAXTIME = 10
+
+	def fit_curve(self):
+		fa = eyemath.fit_dsine(self.tv[0], self.tv[1])
+		if fa != None:
+			pa = fa[1]
+			g.line(self.tv[0], fa[0],1)
+			self.msg(_('Angular velocity = %5.2f rad/sec. Damping Factor = %5.3f')%(pa[1], pa[4]))
+		else:
+			self.msg(_('Failed to fit data'))
+
+	def xmgrace(self):
+		if self.running == True:
+			return
+		p.grace([self.tv])
+
+	def start(self):
+		self.running = True
+		self.index = 0
+		self.tv = [ [], [] ]
+		try:
+			self.MAXTIME = int(DURATION.get())
+			g.setWorld(0, self.MINY, self.MAXTIME, self.MAXY,_('Time'),_('Volt'))
+			Dur.config(state=DISABLED)
+			self.msg(_('Starting the Measurements'))
+			root.after(self.TIMER, self.update)
+		except:
+			self.msg(_('Failed to Start'))
+
+	def stop(self):
+		self.running = False
+		Dur.config(state=NORMAL)
+		self.msg(_('User Stopped the measurements'))
+
+	def update(self):
+		if self.running == False:
+			return
+		t,v = p.get_voltage_time(1)  # Read A2
+		if len(self.tv[0]) == 0:
+			self.start_time = t
+			elapsed = 0
+		else:
+			elapsed = t - self.start_time
+		self.tv[0].append(elapsed)
+		self.tv[1].append(v)
+		if len(self.tv[0]) >= 2:
+			g.delete_lines()
+			g.line(self.tv[0], self.tv[1])
+		if elapsed > self.MAXTIME:
+			self.running = False
+			Dur.config(state=NORMAL)
+			self.msg(_('Completed the Measurements'))
+			return 
+		root.after(self.TIMER, self.update)
+
+	def save(self):
+		try:
+			fn = filename.get()
+		except:
+			fn = 'pendulum.dat'
+		p.save([self.tv],fn)
+		self.msg(_('Data saved to %s')%fn)
+
+	def clear(self):
+		if self.running == True:
+			return
+		self.tv = [ [], [] ]
+		g.delete_lines()
+		self.msg(_('Cleared Data and Trace'))
+
+	def msg(self,s, col = 'blue'):
+		msgwin.config(text=s, fg=col)
