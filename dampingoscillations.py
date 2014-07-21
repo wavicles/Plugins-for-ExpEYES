a9df993cbafc5c87f4129113c14fa29f2f292a61
wavicles <wavicles@users.noreply.github.com>
2014-07-21 21:21:42 +0530

    Update dampingoscillations.py

--- dampingoscillations.py
+++ dampingoscillations.py
@@ -30,10 +30,7 @@ class Spring:
 		else:
 			self.msg(_('Failed to fit data'))
 
-	def xmgrace(self):
-		if self.running == True:
-			return
-		p.grace([self.tv])
+	
 
 	def start(self):
 		self.running = True
