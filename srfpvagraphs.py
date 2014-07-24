a38728bd7e68a995b27976e7b7202177388f4937
wavicles <wavicles@users.noreply.github.com>
2014-07-24 22:26:36 +0530

    Update srfpvagraphs.py

--- srfpvagraphs.py
+++ srfpvagraphs.py
@@ -23,3 +23,39 @@ while et < 15:
 	print s
 	time.sleep(0.1)
 # Calculate Velocity and Acceleration -----------------------------------------
+print "Calculating velocity and acceleration..."
+va = []
+aa = []
+for i in range(0,len(da)-1):
+    # Calculate Velocity
+    v = (da[i+1]-da[i])/(ta[i+1]-ta[i])
+    va.append(v)
+    
+    # Calculate Acceleration
+    if i < len(da)-2:
+        a = (da[i+2]-2*da[i+1]+da[i])/((ta[i+2]-ta[i+1])*(ta[i+1]-ta[i]))
+        aa.append(a)
+
+#Create Plots ----------------------------------------------------------------
+
+subplot(3,1,1)
+plot(ta,da,'r')   #Acceleration Plot
+title('Position-Time Graph')
+xlabel('Time')
+ylabel('Position')
+
+subplot(3,1,2)
+plot(ta[1:],va)
+title('Velocity-Time Graph')
+xlabel('time')
+ylabel('Velocity')
+
+subplot(3,1,3)
+plot(t[2:],aa)
+title('Acceleration of IR')
+xlabel('time')
+ylabel('Acceleration')
+
+show()
+
+
