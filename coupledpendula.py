53bf892e439d3e23ff34cde9de4a7d51f1383730
wavicles <wavicles@users.noreply.github.com>
2014-07-30 20:56:16 +0530

    Update coupledpendula.py

--- coupledpendula.py
+++ coupledpendula.py
@@ -2,6 +2,11 @@
 connect motor of pendulum1 to IN and GND, OUT to A1
 Connect motor of pendulum2 to IN and GND of another ExpEYES box. Connect OUT to Channel A2 of the first ExpEYES box.
 Note: Second ExpEYES box must be connected and detected by CROPLUS program, then run coupledpendula.py so that first ExpEYES box will be detected.
+
+This experiment can be done in three different ways 
+1. both the pendula oscillating in-phase
+2. Both the pendula oscillating out-of-phase
+3. One pendulum at rest while other is set in to oscillations.
 '''
 
 from pylab import *
