9a57dc32fec5699898d241b18376a550f9da7b92
wavicles <wavicles@users.noreply.github.com>
2014-08-10 12:42:40 +0530

    Create springoscillations.py
    To be completed .....remove eco module and add capture from either channel A1 or A2

---
+++ springoscillations.py
@@ -0,0 +1,38 @@
+from pylab import *
+import expeyes.eyesj, time
+p = expeyes.eyesj.open()
+
+p.set_state(10,1)
+
+f = open('srf.dat','r+')
+
+ta = []
+da = []
+strt = time.time()
+
+et =0
+while et < 30:
+dist = p.srfechotime(8,3) # for using two srf modules use 8,3 or 9,0 channels
+et = time.time() - strt
+ta.append(et)
+da.append(dist)
+s = '%5.3f\t %d'%(et,dist)
+#s = '%d\t %d'%(et,dist)
+f.write(s + '\n')
+print s
+time.sleep(0.1)
+
+print p.get_frequency(3) # to get frequency at pin 3 this is giving time out error
+'''
+to fit the sine wave for spring expt.
+'''
+import expeyes.eyemath as em
+p = expeyes.eyesj.open()
+t,v= p.capture(3,400,100)
+vfit, par = em.fit_sine(t,v)
+print par[1] # second parameter is frequency
+print(t,v)
+plot(t, vfit)
+show()
+plot(ta,da)
+show()
