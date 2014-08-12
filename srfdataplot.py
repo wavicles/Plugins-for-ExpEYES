d47672cf212469fabb8b3262f231c0a37b6f1b7f
wavicles <wavicles@users.noreply.github.com>
2014-08-12 22:55:03 +0530

    Update srfdataplot.py

--- srfdataplot.py
+++ srfdataplot.py
@@ -1,3 +1,13 @@
+'''
+expEYES program
+Developed as a part of GSoC Project "Plugins-for-ExpEYES"
+
+'''
+import gettext
+gettext.bindtextdomain("expeyes")
+gettext.textdomain('expeyes')
+_ = gettext.gettext
+
 import numpy as np
 import matplotlib.pyplot as plt
 data = np.loadtxt('srf.dat')  # uses data from srf.dat
